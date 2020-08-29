9050bb69b5ce648b9e75c197a31a806ddfb567fb
MKS-01 <manish10mks@gmail.com>
2020-08-30 01:44:23 +0530

    icon added

--- src/scenes/home/index.js
+++ src/scenes/home/index.js
@@ -4,11 +4,15 @@ import {RootSafeAreaView, RootScrollView} from '_styles/rootView';
 import {useNavigation} from '@react-navigation/native';
 import SlotCard from '_atoms/SlotCard';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+const myIcon = <Icon name="rocket" size={30} color="#900" />;
+
 const HomeScreen = () => {
   const navigation = useNavigation();
   return (
     <RootSafeAreaView>
       <RootScrollView>
+        {myIcon}
         {/* <Button
           onPress={() => navigation.navigate('Details')}
           title="Open Modal"
--- src/styles/SlotCard.js
+++ src/styles/SlotCard.js
@@ -1,13 +1,14 @@
 import styled from 'styled-components/native';
 import {scaleSize, scaleFont} from './mixins';
-import {WHITE} from './colors';
+import {WHITE, GRAY_LIGHT} from './colors';
+import {Platform} from 'react-native';
 
 export const Card = styled.View`
   width: 100%;
   height: ${scaleSize(60)}px;
   background-color: ${WHITE};
-
   border-radius: ${scaleSize(5)}px;
   box-shadow: ${scaleSize(5)}px ${scaleSize(5)}px ${scaleSize(10)}px #dfdccd;
-  /* elevation: 1; */
+  border-width: ${Platform.OS === 'ios' ? '0px' : '1px'};
+  border-color: ${GRAY_LIGHT};
 `;
