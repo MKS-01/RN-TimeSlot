f43b054276bb5c715f4625a91aae77f22e8bb309
MKS-01 <manish10mks@gmail.com>
2020-08-30 23:55:15 +0530

    ss added

--- src/scenes/photo/index.js
+++ src/scenes/photo/index.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {RootSafeAreaView, RootView} from '_styles/rootView';
-import {Text, Container, Button, BtnText} from '_styles/PhotoGallery';
+import {
+  Text,
+  Container,
+  Button,
+  BtnText,
+  ModalContainer,
+  Image,
+  ModalBtn,
+} from '_styles/PhotoGallery';
+import Lightbox from 'react-native-lightbox';
+import {Modal} from 'react-native';
 
 const Photo = () => {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <RootSafeAreaView>
       <RootView>
         <Text>Photo Gallery</Text>
         <Container>
-          <Button onPress={() => console.log('')}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              console.log('Modal has been closed.');
+            }}>
+            <ModalContainer>
+              <Image source={require('../../assets/images/iron.png')} />
+              <Button
+                onPress={() => {
+                  setModalVisible(!modalVisible);
+                }}>
+                <Text>Hide Modal</Text>
+              </Button>
+            </ModalContainer>
+          </Modal>
+
+          <Button
+            onPress={() => {
+              setModalVisible(true);
+            }}>
             <BtnText>Open</BtnText>
           </Button>
         </Container>
--- src/styles/PhotoGallery.js
+++ src/styles/PhotoGallery.js
@@ -26,3 +26,25 @@ export const BtnText = styled.Text`
   font-weight: 600;
   color: #fafafa;
 `;
+
+export const ModalContainer = styled.View`
+  /* display: flex; */
+  align-items: center;
+  justify-content: center;
+  height: 50%;
+  flex: 1;
+  margin: 22px 0px;
+`;
+
+export const Image = styled.Image`
+  height: 400px;
+  width: 100%;
+  border-width: 10px;
+  border-color: #fff;
+`;
+
+export const ModalBtn = styled.TouchableOpacity`
+  padding: ${scaleSize(15)}px ${scaleSize(30)}px;
+  background-color: #2e7d32;
+  border-radius: 10px;
+`;
