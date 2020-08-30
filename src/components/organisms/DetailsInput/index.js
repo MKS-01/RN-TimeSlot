0f9b8ce1cb013a06ffe46954c4fb1e1f4d4474f9
MKS-01 <manish10mks@gmail.com>
2020-08-30 18:27:10 +0530

    dummy data removed

--- src/components/organisms/DetailsInput/index.js
+++ src/components/organisms/DetailsInput/index.js
@@ -68,28 +68,6 @@ const BTN = (props) => {
     RootNavigation.goBack('Home');
   };
 
-  const dummy = {
-    slotData: [
-      {id: 1, slot: '9AM - 10AM', icon: 'clock-time-nine', details: null},
-      {
-        id: 2,
-        slot: '10AM - 11AM',
-        icon: 'clock-time-ten',
-        details: {firstName: 'Dev', lastName: 'Test', phone: 643743},
-      },
-      {
-        id: 3,
-        slot: '11AM - 12AM',
-        icon: 'clock-time-eleven',
-        details: {firstName: 'Dev', lastName: 'Test', phone: 643743},
-      },
-      {id: 4, slot: '12AM - 1PM', icon: 'clock-time-twelve', details: null},
-      {id: 5, slot: '1PM - 2PM', icon: 'clock-time-one', details: null},
-      {id: 6, slot: '2PM - 3PM', icon: 'clock-time-two', details: null},
-      {id: 7, slot: '3PM - 4PM', icon: 'clock-time-three', details: null},
-      {id: 8, slot: '4PM - 5PM', icon: 'clock-time-four', details: null},
-    ],
-  };
   const onSave = (data) => {
     let tempData = slotData;
 
